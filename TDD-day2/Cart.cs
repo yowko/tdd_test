59511b933d1c05ef86047ebbdd01d4c2a9212be7
yowkotsai <yowkotsai@gmail.com>
2017-06-06 00:00:54 +0800

    temp commit

--- TDD-day2/Cart.cs
+++ TDD-day2/Cart.cs
@@ -1,20 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace TDD_day2
 {
     internal class Cart<Book>
     {
-        private Rule<Book> rules;
+        private readonly Rule<Book> rules;
 
         public Cart(Rule<Book> rules)
         {
             this.rules = rules;
         }
 
-        internal object Checkout(List<Book> cart, Func<Book, int> p)
+        internal int Checkout(List<Book> cart, Func<Book, int> selector)
         {
-            throw new NotImplementedException();
+            int result =GetAmount(cart, selector);
+            //var typeCount = cart.Select(a => a).Distinct();
+            
+
+            return result;
+        }
+
+        private int GetAmount(List<Book> cart, Func<Book, int> selector)
+        {
+            int result = 0;
+            if (cart.Select(a=>a).Distinct().Count()==1)
+            {
+                return cart.Sum(selector);
+            }
+            else
+            {
+                int+=GetAmount()
+            }
+            return result;
         }
     }
 }
