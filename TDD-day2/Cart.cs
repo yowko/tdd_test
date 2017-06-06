892fa70e45726a1fee6d82cc43cb1870e102aa55
yowko <yowko0729@gmail.com>
2017-06-06 16:46:46 +0800

    重構 production code

--- TDD-day2/Cart.cs
+++ TDD-day2/Cart.cs
@@ -16,9 +16,6 @@ namespace TDD_day2
         internal double Checkout(List<T> cart, Func<T, double> selector)
         {
             double result = GetAmount(cart, selector);
-            //var typeCount = cart.Select(a => a).Distinct();
-
-
             return result;
         }
 
@@ -26,25 +23,21 @@ namespace TDD_day2
         {
             double result = 0;
             var DistinctCart = cart.Select(a => a).Distinct();
-            if (DistinctCart.Count() == 0)
-                return 0;
-            else if (DistinctCart.Count() == 1)
-            {
-                return cart.Sum(selector);
-            }
-            else
+
+            double discount = 0.0;
+            if (rules.Details.TryGetValue(DistinctCart.ToList().Count(), out discount))
             {
                 var disCartList = DistinctCart.ToList();
                 foreach (var item in disCartList)
                 {
                     cart.Remove(item);
                 }
-
-                double discount = 0.0;
-                rules.Details.TryGetValue(disCartList.Count(), out discount);
                 result = disCartList.Sum(selector) * (1 - discount);
                 result += GetAmount(cart, selector);
             }
+            else
+                return cart.Sum(selector);
+
             return result;
         }
     }
